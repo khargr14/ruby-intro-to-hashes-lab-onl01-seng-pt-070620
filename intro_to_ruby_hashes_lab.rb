f4ee46884b61deba9a80e304b2904ce0e55492a4
Keesha Hargrove <kharg14@gmail.com>
2020-07-17 23:51:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,55 +1,6 @@
-def base_hash
-  monopoly = {
-    :railroads => {}
-  }
+def new_hash
+  test_hash = Hash.new
 end
 
-def monopoly_with_second_tier
-  monopoly = {
-    :railroads => {
-      :pieces => 4
-    }
-  }
-end
-
-def monopoly_with_third_tier
-  monopoly = {
-    :railroads => {
-      :names => {
-        :reading_railroad => {}
-        :pennsylvania_railroad => {}
-        :b_and_o_railroad => {}
-        :shortline_railroad => {}
-      }
-      :pieces => 4
-      :rent_in_dollars => {
-        :one_piece_owned => 25,
-        :two_pieces_owned =>50, 
-        :three_pieces_owned => 100, 
-        :four_pieces_owned => 200
-      }
-    }
-  }
-end
-
-def monopoly_with_fourth_tier
-  monopoly = {
-    :railroads => {
-      :names => {
-        :reading_railroad => "$100",
-        :pennsylvania_railroad => "$200",
-        :b_and_o_railroad => "$400",
-        :shortline_railroad => "$800",
-      }
-      :pieces => 4
-      :rent_in_dollars => {
-        :one_piece_owned => 25,
-        :two_pieces_owned =>50, 
-        :three_pieces_owned => 100, 
-        :four_pieces_owned => 200
-      }
-    }
-  }
-end
 
 
